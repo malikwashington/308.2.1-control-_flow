2cf7645ab6e85cecdc9e4960f83374fcf460df03
malikwashington <112908272+malikwashington@users.noreply.github.com>
2024-09-20 15:47:02 -0400

    added part2

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 
-function plantGrowth(weeks){
+function plantGrowth(weeks, start=20, r=5){
 
         const PI = 3.1415;
-        const area = PI * 5**2
+        const area = PI * r**2
 
         const min = .8;
-        const start = 20
         const max = area / .8
 
         let i = 0;
         let num;
 
+        if(weeks== 0){
+                while(i<=10){
+                        if(i==10) return `After 10 weeks we would have ${num} plants, taking up ${num*.8-area} sq meters more space.
+If the space had remained circular it's approximate radius would be ${Math.sqrt((num*.8)/PI)} meters.\n`
+                        num??=start
+                        num*=2
+                        i++
+                }
+        }
+
         while(i <= weeks){
                 num ??= start
                 console.log(i>0? `The number of plants after week ${i} is ${num}.`: `We are starting with ${num} plants.`)
-                if(.8*max < num) return `The number of plants is more than 80% capacity, we should start PRUNING.` 
+                if(.8*max < num) return `The number of plants is more than 80% capacity, we should start PRUNING.\n`
 
                 console.log(max/2 >= num? "The plants are growing at an acceptable rate, we should continue to monitor them." :
                 "There is room to plant more plants.")
@@ -25,3 +34,4 @@ function plantGrowth(weeks){
 
 
 console.log(plantGrowth(3))
+console.log(plantGrowth(0))
